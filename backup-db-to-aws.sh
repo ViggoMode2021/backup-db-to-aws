cde45ea2c4386d37c1379a229a94bd4279c8c92d
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-02-15 20:05:54 -0500

    Update backup-db-to-aws.sh

--- backup-db-to-aws.sh
+++ backup-db-to-aws.sh
@@ -7,5 +7,6 @@ FILE="database-backups-${now}.csv"
 sudo docker exec -u postgres ${CONTAINER} psql -d ${DB} -c "COPY ${TABLE} TO STDOUT WITH CSV HEADER " > ${FILE}
 sudo mkdir -p db-backups
 sudo mv ${FILE} db-backups
-aws s3api put-object --bucket vig-db-backups --key /home/ubuntu/db-backups/${FILE} --body /home/ubuntu/db-backups/${FILE} #edit s3 bucket 
+aws s3api put-object --bucket vig-db-backups --key /home/ubuntu/db-backups/${FILE} --body /home/ubuntu/db-backups/${FILE}
+aws s3 ls s3://vig-db-backups --recursive --human-readable --summarize 
 # Crontab syntax: 0 0 */10 * * /usr/bin/env bash ./send-table-to-csv >/dev/null 2>&1
